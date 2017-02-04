ec650791a9b2ab371e7e9d63164e827a7909d83c
Sands Fish <sandsfish@gmail.com>
2017-02-04 04:36:34 -0500

    Adding integration from Media Cloud API to Javascript/Three.js

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask import Flask
+from flask import jsonify
 
 #from networkx.readwrite import json_graph
 #import networkx as nx
@@ -7,7 +8,7 @@ from flask import Flask
 #import numpy as np
 #import datetime
 #import requests
-#import mediacloud
+import mediacloud
 import json
 import os
 import copy
@@ -17,16 +18,23 @@ import copy
 # from mediacloudlandscape.landscape import *
 
 mc = None
+app = Flask(__name__)
 
 # Berkman Projects Directory
 def init():
-	return
-#    berkman_projects = os.environ['BKP']
-#    api_key = cPickle.load( file( os.path.expanduser( berkman_projects + '/MediaCloud/mediacloud_api_key.pickle' ), 'r' ) )
-#    mc = mediacloud.api.MediaCloud(api_key)
-#    print(api_key)
+   global mc
+   api_key = 'c300df092175a8c3e2c3b5638a3bdbd80214be36581b498d85b1e6d14146748f'
+   mc = mediacloud.api.MediaCloud(api_key)
+   print(api_key)
+   print(mc)
+   # print('Calling for data...')
+   # top_media = mc.mediaList(timespans_id=1467)
+   # top_media = mc.topicMediaList(1477)
+   # print(json.dumps(top_media))
+   # print('Called')
+   #    berkman_projects = os.environ['BKP']
+   #    api_key = cPickle.load( file( os.path.expanduser( berkman_projects + '/MediaCloud/mediacloud_api_key.pickle' ), 'r' ) )
 
-app = Flask(__name__)
 
 # @app.route('/')
 # def index():
@@ -37,6 +45,14 @@ app = Flask(__name__)
 def projects(name):
 	print 'Name: {0}'.format(name)
 	return render_template('/{0}'.format(name))
+
+@app.route('/topic_media/<int:topic_id>')
+def topic_media(topic_id):
+	# print('Calling for data...')
+	top_media = mc.topicMediaList(topic_id)
+	# top_media = mc.mediaList(timespans_id=1467)
+	# json.dumps(top_media)
+	return render_template('mc_topic_media.html', data=top_media)
 
 if __name__ == '__main__':
     init()
